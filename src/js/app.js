06fa3e48edd7a1afc54690c61d7411805ed3c5da
Valentyn Dorosh <valentyn.dorosh@altium.biz>
2018-12-05 10:54:13 +0200

    updates and fixes

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,6 @@
 import svg from 'svg4everybody'
 svg()
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (!('ontouchstart' in window)) document.querySelector('body').classList.add('no-touch')
+})
